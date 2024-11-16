9403d232d847a32049c177f861c41c43058d9c46
Mina Yousefnezhad <93423309+Minay98@users.noreply.github.com>
2024-11-16 13:04:55 +0330

    chapter 6

--- chapter6/serial.c
+++ chapter6/serial.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
-#include <time.h>
+#include <time.h> 
 
 void prefix_sum_serial(int a[], int b[], int n) {
-    b[0] = a[0]; 
+    b[0] = a[0];
     for (int i = 1; i < n; i++) {
-        b[i] = b[i - 1] + a[i]; 
+        b[i] = b[i - 1] + a[i];
     }
 }
 
 int main() {
-    int a[] = {1, 2, 1, 5, 0, 7, 3, 4, 2, 7}; 
-    int n = sizeof(a) / sizeof(a[0]);         
-    int b[n];                               
+    int n = 500000; 
+    int a[n], b[n];
+
+    for (int i = 0; i < n; i++) {
+        a[i] = 1; 
+    }
 
     clock_t start, end;
-    
+
     start = clock();
-
     prefix_sum_serial(a, b, n);
-
     end = clock();
 
-    double execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("execution time: %lf s\n", execution_time);
-
-    printf("Array a: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
-    printf("Array b: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", b[i]);
-    }
-    printf("\n");
-
+    printf("Execution Time: %lf s\n", ((double)(end - start)) / CLOCKS_PER_SEC);
     return 0;
 }
--- chapter6/thread.c
+++ chapter6/thread.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 
-#define N 10 // Size of the array
-
-// Structure to hold data for each thread
 typedef struct {
     int *a;
     int *b;
@@ -12,7 +10,6 @@ typedef struct {
     int end;
 } ThreadData;
 
-// Function to calculate prefix sum in parallel
 void *prefix_sum_partial(void *arg) {
     ThreadData *data = (ThreadData *)arg;
     data->b[data->start] = data->a[data->start];
@@ -22,7 +19,6 @@ void *prefix_sum_partial(void *arg) {
     return NULL;
 }
 
-// Function to merge the results of two chunks
 void merge_chunks(int b[], int mid, int end) {
     int adjust_value = b[mid];
     for (int i = mid + 1; i <= end; i++) {
@@ -31,53 +27,32 @@ void merge_chunks(int b[], int mid, int end) {
 }
 
 int main() {
-    int a[] = {1, 2, 1, 5, 0, 7, 3, 4, 2, 7}; // Initial array
-    int n = sizeof(a) / sizeof(a[0]); // Size of the array
-    int b[n]; // Result array
+    int n = 500000; 
+    int a[n], b[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = 1; 
+    }
 
     pthread_t thread1, thread2;
     ThreadData data1, data2;
 
-    // Divide the array into two halves
     int mid = n / 2 - 1;
+
     data1 = (ThreadData){a, b, 0, mid};
     data2 = (ThreadData){a, b, mid + 1, n - 1};
 
     clock_t start, end;
-    
-    // Start measuring time
-    start = clock();
 
-    // Create two threads for parallel execution
+    start = clock();
     pthread_create(&thread1, NULL, prefix_sum_partial, &data1);
     pthread_create(&thread2, NULL, prefix_sum_partial, &data2);
 
-    // Wait for both threads to finish
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-    // Merge the chunks
     merge_chunks(b, mid, n - 1);
-
-    // End measuring time
     end = clock();
 
-    // Calculate and display execution time
-    double execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Parallel execution time: %lf seconds\n", execution_time);
-
-    // Display arrays a and b
-    printf("Array a: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
-    printf("Array b: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", b[i]);
-    }
-    printf("\n");
-
+    printf("Execution Time: %lf s\n", ((double)(end - start)) / CLOCKS_PER_SEC);
     return 0;
 }
